2e51e88ae198502f0cdec2792866888708190699
juratsobirov <juratsobirov72@gmail.com>
2024-08-20 11:27:16 +0500

    Initial commit

---
+++ lib/pages/upload_page.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ngdemo13/services/http_service.dart';
+import 'package:ngdemo13/services/log_service.dart';
+
+import '../models/cat_upload_res.dart';
+
+class UploadPage extends StatefulWidget {
+  const UploadPage({super.key});
+
+  @override
+  State<UploadPage> createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  bool isLoading = false;
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
+  _apiUploadCat() async {
+    if (_image == null) return;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    var imageFile = File(_image!.path);
+    var response = await Network.MUL(Network.API_CAT_UPLOAD, imageFile, Network.paramsEmpty());
+    CatUploadRes catUploadRes = Network.parseCatUpload(response!);
+    LogService.i(response!);
+
+    setState(() {
+      isLoading = false;
+    });
+
+    backToFinish();
+  }
+
+  backToFinish(){
+    Navigator.of(context).pop(true);
+  }
+
+  Future getImageFromGallery() async {
+    final XFile? image = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 100,
+    );
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future getImageFromCamera() async {
+    final XFile? image = await _picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 100,
+    );
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Upload Cat"),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 20),
+                    color: Colors.blue,
+                    width: 300,
+                    height: 200,
+                    child: _image == null
+                        ? const Center(child: Text('No image selected.'))
+                        : Image.file(
+                      File(_image!.path),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MaterialButton(
+                        color: Colors.blue,
+                        onPressed: () {
+                          getImageFromGallery();
+                        },
+                        child: Text("Get Image"),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      MaterialButton(
+                        color: Colors.blue,
+                        onPressed: () {
+                          _apiUploadCat();
+                        },
+                        child: Text("Upload Cat"),
+                      ),
+                    ],
+                  ),
+                  MaterialButton(
+                    onPressed: (){
+                      getImageFromCamera();
+                    },
+                    color: Colors.amber,
+                    child:Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Get Camera", style: TextStyle(fontSize: 16),),
+                        SizedBox(width: 5,),
+                        Icon(Icons.linked_camera_outlined),
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            isLoading
+                ? Center(
+              child: CircularProgressIndicator(),
+            )
+                : SizedBox.shrink(),
+          ],
+        ));
+  }
+}
